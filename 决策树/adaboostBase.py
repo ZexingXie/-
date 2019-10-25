7881d32376b81225e4cb91b716b87097a06135af
XingShell <1114511459@qq.com>
2019-10-25 08:59:16 +0800

    修改

--- 决策树/adaboostBase.py
+++ 决策树/adaboostBase.py
@@ -45,7 +45,7 @@ class AdaboostStumps:
 #     for index,x in enumerate(x_datas):
 #         ModelTree(x)
 
-def AdaBoost(x_data, y_data,rounds=25):
+def AdaBoost(x_data, y_data,rounds=50):
     ms_list = []
     a = []
     N = len(y_data)
@@ -54,21 +54,27 @@ def AdaBoost(x_data, y_data,rounds=25):
     determined_dimension, e_in_result, theta_result, s_result = decision_stump_multi_d(x_data, y_data,W)
     Model = (determined_dimension, theta_result, s_result)
     ms = ModelStump(*Model)
+    errorIndex = ms.errorLabels(x_data, y_data)
+    e_w = len(errorIndex)/len(y_data)
     for t in range(rounds):
         # 重新计算一遍错误向量
         errorIndex = ms.errorLabels(x_data,y_data)
         error_ratio = len(errorIndex)/len(y_data)
         if error_ratio == 0.5:
-            break
-        am = (1 / 2) * np.log((1 - error_ratio) / error_ratio)
+            return -1,None
+        # print(e_w)
+        # am = (1 / 2) * np.log((1 - error_ratio) / error_ratio)
+        am = (1 / 2) * np.log((1 - e_w) / e_w)
         # if error_ratio<0.5:
         # print(errorIndex)
         a.append(am)
         ms_list.append(ms)
         am = np.abs(am)
+        e_w = 0
         for i in range(len(y_data)):
             if i in errorIndex:
                 W[i] *= np.exp(am)
+                e_w += W[i]
             else:
                 W[i] *= np.exp(-am)
         sum = np.sum(W)
@@ -81,9 +87,10 @@ def AdaBoost(x_data, y_data,rounds=25):
 
     adaboost = AdaboostStumps(a,ms_list)
     e_in_result = adaboost.accurate(x_data,y_data)
-    print(a)
+    # print(a)
     print("E_IN:", e_in_result)
-    return adaboost
+
+    return e_in_result,adaboost
 
 
 if __name__ == '__main__':
@@ -91,12 +98,28 @@ if __name__ == '__main__':
     start = time.time()
     x_train, y_train = load_data('data/train.txt')
     x_test, y_test = load_data('data/test.txt')
+    _, Model = AdaBoost(x_train, y_train)
+    # t = x_train,y_train
+    # x_train, y_train = x_test,y_test
+    # x_test, y_test = t
+    yt = []
+    ytest = []
+    for r in range(1,1000):
+        # Model = AdaBoost(x_train,y_train,r)
+        y,Model = AdaBoost(x_train,y_train,r)
 
-    t = x_train,y_train
-    x_train, y_train = x_test,y_test
-    x_test, y_test = t
+        if y==-1:
+            break
+        yt.append(y)
+        ytest.append(Model.accurate(x_test,y_test))
+    #
+    import matplotlib.pyplot as plt  # 约定俗成的写法plt
+    plt.plot(yt)
+    plt.plot(ytest)
+    plt.show()
 
-    Model = AdaBoost(x_train,y_train)
+
+
     print("E_OUT:",Model.accurate(x_test,y_test))
-    # print("E_OUT:", err_out_counter(x_test, y_test, s_result, theta_result, determined_dimension))
+
 
